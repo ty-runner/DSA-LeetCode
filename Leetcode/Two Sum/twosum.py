8c9eef1e9a9c5d8193ff42655366701ff4fb92c0
ty-runner <ty.runner02@gmail.com>
2023-11-28 19:06:46 -0500

    Create twosum.py

---
+++ Leetcode/Two Sum/twosum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        numToIndex = {}
+        for i in range(len(nums)):
+            if target - nums[i] in numToIndex:
+                return [numToIndex[target - nums[i]], i]
+            numToIndex[nums[i]] = i
+        

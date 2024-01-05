28fdbd4ba59df1936d8fb475899ca6cda4de8401
ty-runner <ty.runner02@gmail.com>
2024-01-05 15:23:32 -0800

    Update find_minimun_in_rotated_sorted_array-153.c

--- BLIND75/find_minimun_in_rotated_sorted_array-153.c
+++ BLIND75/find_minimun_in_rotated_sorted_array-153.c
@@ -1,12 +1,11 @@
 int findMin(int* nums, int numsSize) {
     int min = nums[0];
     int last = nums[0];
-    for(int i = (numsSize/2)-1; i<numsSize; i++){
-        min = nums[i];
+    for(int i = (numsSize/2); i<numsSize; i++){
+        last = nums[i];
         if(min>last){
             min = last;
         }
-        last = nums[i];
     }
     return min;
 }

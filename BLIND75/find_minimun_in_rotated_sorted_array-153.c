71019b44be270eec343694acf9e006c9fbefa321
ty-runner <ty.runner02@gmail.com>
2024-01-05 15:23:45 -0800

    Update find_minimun_in_rotated_sorted_array-153.c

--- BLIND75/find_minimun_in_rotated_sorted_array-153.c
+++ BLIND75/find_minimun_in_rotated_sorted_array-153.c
@@ -1,11 +1,12 @@
 int findMin(int* nums, int numsSize) {
-    int min = nums[0];
+    if(numsSize == 0){
+        return nums[0];
+    }
+    int min = nums[numsSize/2];
     int last = nums[0];
-    for(int i = (numsSize/2); i<numsSize; i++){
-        last = nums[i];
-        if(min>last){
-            min = last;
-        }
+    if(nums[min+1] < min){
+        return nums[min+1];
     }
     return min;
 }
+//at the half way point, if the next num is less than the last, then that is the smallest num, if it's greater, then either the number is further back or that middle number

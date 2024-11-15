382df5d0c980cc06bc91094c4bbd99d138059256
ty-runner <ty.runner02@gmail.com>
2024-11-15 13:43:47 -0800

    strings chapter

---
+++ LearnC++.org/strings_exercise.cpp
@@ -0,0 +1,11 @@
+#include<iostream> 
+// you may need to include something else
+#include<string>
+using namespace std; 
+int main() 
+{     
+    //Your code here...
+	string response = "I just learned something about strings";
+    cout << response << endl;
+    return 0;
+}
